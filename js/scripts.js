27e4defa177b40a58eea0b2b0a6613300b35a6f3
Andrew Glines and Jordan Loop <jordan@>
2017-05-04 11:56:02 -0700

    Add new for loop to keep track of which column (ones ,tens, etc.) is being processed, to be used later

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,36 @@
 // BACK END LOGIC
 
 var translation = function(arabicInput){
-
+var i = 0;
 var outputArray = [];
 var digits = ["1","2","3","4","5","6","7","8","9"];
+// debugger
 
-// later:  check <4000
 // later: check actual number input
+// later:  check <4000
 
-// arabicInput is a string
 // split it into an array to loop over
 arabicArray = arabicInput.split("");
 // reverse array so we process ones first
 arabicArray.reverse();
-// debugger
-// for all elmts in user input "arabicArray",
+
+// for all elmts in user input "arabicArray"
+for (var j = 0; j <= (arabicArray.length - 1) ; j++ ) {
+
 // loop thru digits 1-9,
-for(var i = 0; i <= digits.length; i++){
-  if (digits[i] === arabicArray[0]) {
-    console.log(digits[i]);
-  }
-}
+  for(var i = 0; i <= digits.length; i++) {
+    // when digit lands on the user input digit,
+    if (digits[i] === arabicArray[j]) {
+      // see which column we're on
+      console.log("column is ", j);
+    }
 
-  // what column are we in
+}; // FOR LOOP DIGITS
 
+}; //  ARABIC ARRAY LOOPs
+// return result ;
 
-    // do a hard-coded assignment
-    // and for R1 in ones column, R=>"I"
-    // if R1 = is tens, Rout = "X", "C".
-
-
-};
-
-// return result;
+}; // FUNCTION
 
 
 
